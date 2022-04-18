d026cc2967c1b46d929ad5de35739f9605896d72
Marcus <marcusdchr@gmail.com>
2022-04-18 16:07:23 +0200

    .

--- belief_base.py
+++ belief_base.py
@@ -1,34 +1,37 @@
-#%%
+from asyncio.windows_events import NULL
 from xml.etree.ElementTree import tostring
 import sympy.logic.boolalg as boolalg
-from sympy.logic.boolalg import And
+from sympy.logic.boolalg import And, Not, Or, Xor
+from utils import get_literals, pl_resolution
 
 class BeliefBase:
     def __init__(self):
-        self.beliefs = []
+        self.beliefs = NULL
 
     def add_belief(self, belief):
-
-        belief = boolalg.to_cnf(belief, simplify=True, force=True)
-        belief = str(belief).split(" & ")
-        for b in belief:
-
-            # Implement check that works with checking parenthesies
-            if b not in self.beliefs:
-                if (len(belief) > 1) and (b):
-                    self.beliefs.append(b[1:-1])
-                else:
-                    self.beliefs.append(b)
-        print(type(belief))
+        if (self.beliefs == NULL):
+            self.beliefs = boolalg.to_cnf(belief)
+        else:
+            self.beliefs = And(self.beliefs, boolalg.to_cnf(belief))
+            self.beliefs = boolalg.to_cnf(self.beliefs)
 
     def ask(self, query):
-        pass
+        query = Not(query)
+        query = boolalg.to_cnf(query)
+        return pl_resolution(self.get_clauses(), query)
+        #pass
+
+    def get_clauses(self):
+        return str(self.beliefs).split(" & ")
 
 bb = BeliefBase()
-bb.add_belief("(A >> B) & (C >> A) ")
+#bb.add_belief("(A >> B) & (C >> A) ")
 bb.add_belief("(A & B)")
-print(bb.beliefs)
+print(bb.ask("A"))
+#print(bb.beliefs)
 #b = And().
-s = str(bb.beliefs[0])
-print(type(bb.beliefs))
-# %%
+#s = str(bb.beliefs[0])
+#print(type(bb.beliefs))
+#clauses = bb.get_clauses()
+#for c in clauses:
+#    print(get_literals(c))
---
+++ utils.py
@@ -0,0 +1,45 @@
+import sympy.logic.boolalg as boolalg
+from sympy.logic.boolalg import And, Not, Or, Xor
+
+def get_literals(clause):
+    clause = clause.replace(" ", "")
+    clause = clause.replace("(", "")
+    clause = clause.replace(")", "")
+    clause = clause.split("|")
+    return clause
+
+def pl_resolution(kb, a):
+    clauses = kb
+    clauses.append(str(a))
+
+    # Make all possible combinations of clauses where literals are subtracted
+    while True:
+        resolution_set = []
+        for i,c in enumerate(clauses):
+            for j,d in enumerate(clauses):
+                if (i != j):
+                    literals_a = get_literals(c)
+                    literals_b = get_literals(d)
+                    for l_a in literals_a:
+                        # If a literal exists and a ~literal exist.
+                        if (str(Not(l_a)) in literals_b):
+                            
+                            clause_to_append = ""
+                            for l_b in literals_b:
+                                if (str(Not(l_a)) != l_b):
+                                    clause_to_append += l_b + " | "
+                            clause_to_append = clause_to_append[:-3]
+                            if(clause_to_append not in resolution_set and clause_to_append not in clauses):
+                                resolution_set.append(clause_to_append)
+        if (len(resolution_set) == 0):
+            break
+        elif ("" in resolution_set):
+            clauses.append("")
+            break
+        else:
+            clauses.extend(resolution_set)
+
+    if ("" in clauses):
+        return True
+    else:
+        return False
