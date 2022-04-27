bb2633f58754ebf61464c16c76c6118ba9197ff7
Marcus <marcusdchr@gmail.com>
2022-04-27 16:40:30 +0200

    Added some comments

--- utils.py
+++ utils.py
@@ -4,7 +4,15 @@ from sympy.logic.boolalg import And, Not, Or, Xor
 import copy
 
 # binary counter
-def binary_counter(n):
+def binary_counter(n : int):
+    """
+    Input: n - number.
+    
+    Return: Returns a list of integers representing the binary 
+    representation of n with little-endian notation (If I remember correctly).
+    
+    Example: binary_counter(5) returns [2, 0]
+    """
     count = list(str(bin(n)[2:])[::-1])
     counter = 0
     output = []
@@ -17,9 +25,12 @@ def binary_counter(n):
 
 class Clause:
     """
-    @param: test
+    Representation of a clause.
     """
     def __init__(self, clause : str, order : int, isString=True):
+        """
+        Input: clause - clause as string. order - int representing the clause importance.
+        """
         self.list_of_literals = []
         self.order = copy.copy(order)
         if(isString):
@@ -34,6 +45,7 @@ class Clause:
             for l in clause:
                 self.list_of_literals.append(l)
 
+    # Returns a sorted string of all the literals in the clause (Used for comparing if two clauses are equivalent).
     def sorted_string(self):
         sorted_list = sorted(self.list_of_literals)
         sorted_list_str = ""
@@ -42,6 +54,7 @@ class Clause:
         return sorted_list_str
         #return ''.join(map(str, sorted(self.list_of_literals)))
     
+    # Used for debug purposes.
     def debug_print(self, res_ret=False):
         output = "[DEBUG CLAUSE]:"
         for l in self.list_of_literals:
@@ -51,21 +64,33 @@ class Clause:
         else:
             return output
 
+    # Overwritten equality operator. Allows comparison of two clauses based on their order number.
     def __eq__(self, other):
         return self.order == other.order
 
+    # Overwritten less-than operator. Allows comparison of two clauses based on their order number.
     def __lt__(self, other):
         return self.order < other.order
 
 class Literal:
+    """
+    Representation of a literal.
+    """
     def __init__(self, literal, order, parent):
+        """
+        Input: literal - string representing the literal, order - int representing the literal importance.
+        """
         self.value = literal
         self.parent = parent
         self.order = order
 
+    # Overwritten equality operator. Allows comparison of two literals based on their value.
+    # Used in the clause class sorted_string method.
     def __eq__(self, other):
         return self.value == other.value
 
+    # Overwritten less-than operator. Allows comparison of two literals based on their value.
+    # Used in the clause class sorted_string method.
     def __lt__(self, other):
         return self.value < other.value
 
@@ -76,50 +101,13 @@ def get_literals(clause):
     clause = clause.split("|")
     return clause
 
-# Useless
-def pl_resolution(kb, a):
+
+def pl_resolution2(kb_input, a):
     """
     Input: Kb - Knowledge base or Belief base, a - question.
     Output: True or False, based on if the question is true or false with the given knowledge.
     The function uses a brute force resolution method to proove if the question is true or false.
     """
-    clauses = kb
-    clauses.append(str(a))
-
-    # Make all possible combinations of clauses where literals are subtracted
-    while True:
-        resolution_set = []
-        for i,c in enumerate(clauses):
-            for j,d in enumerate(clauses):
-                if (i != j):
-                    literals_a = get_literals(c)
-                    literals_b = get_literals(d)
-                    for l_a in literals_a:
-                        # If a literal exists and a ~literal exist.
-                        if (str(Not(l_a)) in literals_b):
-                            
-                            clause_to_append = ""
-                            for l_b in literals_b:
-                                if (str(Not(l_a)) != l_b):
-                                    clause_to_append += l_b + " | "
-                            clause_to_append = clause_to_append[:-3]
-                            if(clause_to_append not in resolution_set and clause_to_append not in clauses):
-                                resolution_set.append(clause_to_append)
-        if (len(resolution_set) == 0):
-            break
-        elif ("" in resolution_set):
-            clauses.append("")
-            break
-        else:
-            clauses.extend(resolution_set)
-
-    if ("" in clauses):
-        return True
-    else:
-        return False
-
-
-def pl_resolution2(kb_input, a):
     a = str(Not(a))
     a = str(boolalg.to_cnf(a))
     a = a.split(" & ")
@@ -128,6 +116,7 @@ def pl_resolution2(kb_input, a):
         kb.append(Clause(c, -1))
     
     is_not_done = True
+    # Make all possible combinations of clauses where literals are subtracted
     while is_not_done:
         is_not_done = False
 
@@ -138,6 +127,8 @@ def pl_resolution2(kb_input, a):
                     # Looking at the literals in each clause to determine if they cancel out.
                     for k,li in enumerate(ci.list_of_literals):
                         for l,lj in enumerate(cj.list_of_literals):
+                            # Check for contradiction.
+                            # Example: If a literal 'A' exists and a ~'A' literal exist. Then this statement is true
                             if (str(Not(li.value)) == lj.value):
                                 copy_of_ci = copy.deepcopy(ci)
                                 copy_of_cj = copy.deepcopy(cj)
@@ -169,6 +160,13 @@ def pl_resolution2(kb_input, a):
 
 # Easy solution - Delete the whole clause that contains outdated information
 def revision(kb_input, a):
+    """
+    Input: Kb - Knowledge base or Belief base, a - question.
+
+    Output: List of clauses that needs to be removed in order not to break our knowledge / belief base.
+
+    The function uses a brute force revision method to proove if the question is true or false.
+    """
     #kb_org = copy.deepcopy(kb_input)
     counter = 0
     clauses_to_remove = []
@@ -188,18 +186,18 @@ def revision(kb_input, a):
         for c in clauses_to_remove:
             #print("Removed clause: ", kb[c].debug_print())
             #print(type(kb[c]))
-            #del kb[c]
-            print("[{1}]This was popped: {0}".format(kb.list_of_clauses.pop(c).list_of_literals[0].value, counter))
+            kb.list_of_clauses.pop(c)
+            #print("[{1}]This was popped: {0}".format(kb.list_of_clauses.pop(c).list_of_literals[0].value, counter))
 
         if (not pl_resolution2(kb.list_of_clauses, Not(a))):
-            print_stuff = ""
-            for c in kb.list_of_clauses:
-                print_stuff += c.debug_print(True)
-            print(print_stuff)
+            #print_stuff = ""
+            #for c in kb.list_of_clauses:
+            #    print_stuff += c.debug_print(True)
+            #print(print_stuff)
             return clauses_to_remove
 
 
-    print("Stuff to remove: ", clauses_to_remove)
-    return clauses_to_remove
+    #print("Stuff to remove: ", clauses_to_remove)
+    #return clauses_to_remove
 
     
